8684a6a83f9238ef2e6ba8d17738d58b1e66d014
kost <solodkovkm1997@gmail.com>
2023-02-10 22:50:29 +0300

    task6

---
+++ homework/task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+isdigit(Convert.ToInt32(Console.ReadLine()));
+
+void isdigit(int num) 
+{
+    if(num%2 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else 
+    {
+        Console.WriteLine("нет");
+    }
+}
