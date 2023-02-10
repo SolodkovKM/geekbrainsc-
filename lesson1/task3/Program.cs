230533131463027df48a56726742ab210faeb4bc
kost <solodkovkm1997@gmail.com>
2023-02-10 21:57:56 +0300

    task3 v2м

--- lesson1/task3/Program.cs
+++ lesson1/task3/Program.cs
@@ -1,9 +1,29 @@
 ﻿Console.WriteLine("Введите число");
-int day = Convert.ToInt32(Console.ReadLine());
-if(day == 1) Console.WriteLine("Понедельник");
-if(day == 2) Console.WriteLine("Вторник");
-if(day == 3) Console.WriteLine("Среда");
-if(day == 4) Console.WriteLine("Четверг");
-if(day == 5) Console.WriteLine("Пятница");
-if(day == 6) Console.WriteLine("Суббота");
-if(day == 7) Console.WriteLine("Воскресение");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Неправильное число");
+        break;
+}
