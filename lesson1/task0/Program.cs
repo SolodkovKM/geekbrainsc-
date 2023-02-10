b7a4a78442e701dd37f6ecebf5bb4de65882c869
kost <solodkovkm1997@gmail.com>
2023-02-10 21:04:31 +0300

    task0 resolved

---
+++ lesson1/task0/Program.cs
@@ -0,0 +1,12 @@
+﻿void msg() {
+    Console.WriteLine("Введите число");
+    var num = Console.ReadLine();
+    if(int.TryParse(num, out int isnum)) {   
+       Console.WriteLine("Квадрат числа:" + isnum*isnum );
+    }
+    else {
+        Console.WriteLine("это не число");
+        msg();
+    }
+}
+msg();
