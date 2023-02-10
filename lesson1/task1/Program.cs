d8320791df162af653cdd58243fd04787da99240
kost <solodkovkm1997@gmail.com>
2023-02-10 21:25:44 +0300

    task1

---
+++ lesson1/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿void issquare()
+{
+    Console.WriteLine("Введите число A: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    if(a == b*b)
+        {
+            Console.WriteLine("Yes");
+        }
+        else {
+            Console.WriteLine("no");
+    }
+}
+issquare();
