ee0f226feb04c41e63798959f9ca2a1e7d0177e4
kost <solodkovkm1997@gmail.com>
2023-02-14 21:29:00 +0300

    task10

---
+++ homework/task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int second = number % 100 / 10;
+Console.WriteLine($"{number} -> {second}");
