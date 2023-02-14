4d055565b672acabf80b91aef3b425934f179359
kost <solodkovkm1997@gmail.com>
2023-02-14 20:16:57 +0300

    lesson2 task0

---
+++ lesson2/task9/Program.cs
@@ -0,0 +1,5 @@
+﻿int random = Random.Shared.Next(10, 100);
+int last = random % 10;
+int first = random / 10;
+if(last > first) Console.WriteLine(last);
+else Console.WriteLine(random + " -> " +first);
