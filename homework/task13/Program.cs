fe47aed8d74e6117f792ebbbccabb456c5f475d8
kost <solodkovkm1997@gmail.com>
2023-02-14 21:55:30 +0300

    task13

---
+++ homework/task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99)
+{
+    int result = num;
+    for(int i = 10; result > 1000;)
+    {
+        result = result / i;
+    }
+    Console.WriteLine($"{num} -> {result % 10}");
+}
+else Console.WriteLine("нет 3 цифры");
