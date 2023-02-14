2c6820f93b0be1e4f44a846f8d4bba1e76494d5f
kost <solodkovkm1997@gmail.com>
2023-02-14 21:02:58 +0300

    task12

---
+++ lesson2/task12/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numA % numB == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно, остаток: {numA % numB}");
