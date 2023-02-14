5290ad3426a8fafb838379893219bd5fb0a0e94b
kost <solodkovkm1997@gmail.com>
2023-02-14 21:17:59 +0300

    task16

---
+++ lesson2/task16/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numA == numB * numB || numB == numA * numA) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
