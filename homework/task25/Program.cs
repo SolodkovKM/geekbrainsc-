f6edea6c25a36bac981a9e64a4675bd81de573f5
kost <solodkovkm1997@gmail.com>
2023-02-18 10:15:54 +0300

    task25

---
+++ homework/task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+void cube(int num) 
+{
+    for(int i = 1; i <= num; i ++ )
+    {
+        Console.Write($"{i*i*i} ");
+    }
+}
+cube(number);
