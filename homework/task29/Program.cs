b632c883d0ac18a61713cfd3876e3171f6177c83
kost <solodkovkm1997@gmail.com>
2023-02-21 22:13:29 +0300

    task29

---
+++ homework/task29/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 8 чисел через Enter");
+int[] FillArray()
+{
+    int[] arr = new int[8];
+    for(int i = 0; i < 8; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void EchoArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(',',array)}]");
+}
+
+EchoArray(FillArray());
