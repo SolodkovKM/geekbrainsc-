d7177a4c4bfe49d58da4b8575ccbb7824bc8b8ad
kost <solodkovkm1997@gmail.com>
2023-02-21 20:55:16 +0300

    task26,tsk28

---
+++ lesson4/task26/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count(int num)
+{
+    int count = 1;
+    do
+    {
+        num /= 10;
+        count++;
+    }
+    while(num / 10 > 0);
+    return count;
+}
+
+Console.WriteLine($"{number} -> {count(number)}");
---
+++ lesson4/task28/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int factor(int num)
+{
+    int fact = 1;
+    int i = 1;
+    while(i <= num)
+    {
+        fact *= i;
+        i++;
+    }
+    return fact;
+}
+
+Console.WriteLine($"{num} -> {factor(num)}");
