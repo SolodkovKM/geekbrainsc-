cf4de84630982a0ed65313ed5e4756d62e39baae
kost <solodkovkm1997@gmail.com>
2023-02-21 21:03:40 +0300

    task251

---
+++ homework/task251/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{A}, {B} -> {Math.Pow(A,B)}");
