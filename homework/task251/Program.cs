9fa53fae42c6f14370d7754b1a67f53da0a65799
kost <solodkovkm1997@gmail.com>
2023-02-21 22:22:25 +0300

    task251/2

--- homework/task251/Program.cs
+++ homework/task251/Program.cs
@@ -2,4 +2,9 @@
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число В");
 int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"{A}, {B} -> {Math.Pow(A,B)}");
+int result = 1;
+for(int i = 1; i <= B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine(result);
