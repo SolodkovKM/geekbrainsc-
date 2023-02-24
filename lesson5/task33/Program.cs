f5ea266a413b377c4aa458f3889716b94040aeb2
kost <solodkovkm1997@gmail.com>
2023-02-24 21:20:13 +0300

    task33

---
+++ lesson5/task33/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+int[] FillArray(int length, int leftnum, int rightnum)
+{
+    int[] rand_arr = new int[length];
+    for (int i = 0; i < rand_arr.Length; i++)
+    {
+        rand_arr[i] = Random.Shared.Next(leftnum, rightnum);
+    }
+    return rand_arr;
+}
+
+bool CheckNum(int[] array, int num)
+{
+    return array.Contains(num);
+}
+
+int[] filled_array = FillArray(10, -9, 10);
+Console.WriteLine($"{string.Join(',', filled_array)} {CheckNum(filled_array, 3)}");
