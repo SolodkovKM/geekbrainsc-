4250ede186ab066194cf780760f85457e7eaee3b
kost <solodkovkm1997@gmail.com>
2023-02-24 22:50:02 +0300

    task38

---
+++ homework/task38/Program.cs
@@ -0,0 +1,17 @@
+﻿
+double DifferenceOfValue(double[] array)
+{
+    double MaxValue = 0;
+    double MinValue = 0;
+    foreach(double i in array)
+    {
+        if(i > MaxValue) MaxValue = i;
+        if(i < MinValue) MinValue = i;
+    }
+    return(MaxValue - MinValue);
+}
+
+
+double[] filled_array = new double[5]{1, 2.2, 1.5, -2.5, -10.8};
+
+Console.WriteLine($"[{string.Join('|', filled_array)}] -> {DifferenceOfValue(filled_array)}");
