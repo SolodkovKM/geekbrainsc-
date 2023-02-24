bd1d9f2e120e6fe3a627f5d3999495c68ec4c2da
kost <solodkovkm1997@gmail.com>
2023-02-24 21:36:19 +0300

    task35

---
+++ lesson5/task35/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] FillArray(int length, int leftnum = int.MinValue, int rightnum = int.MaxValue)
+{
+    int[] rand_arr = new int[length];
+    for (int i = 0; i < rand_arr.Length; i++)
+    {
+        rand_arr[i] = Random.Shared.Next(leftnum, rightnum);
+    }
+    return rand_arr;
+}
+
+int[] filled_array = FillArray(123);
+
+int Count(int[] array, int leftnum = int.MinValue, int rightnum = int.MaxValue)
+{
+    int Count = 0;
+    foreach(int i in array)
+    {
+        if(i > leftnum && i < rightnum) Count++;
+    }
+    return Count;
+}
+Console.WriteLine($"{string.Join(',', filled_array)} -> {Count(filled_array, 10, 100)}");
