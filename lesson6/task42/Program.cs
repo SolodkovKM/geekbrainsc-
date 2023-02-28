af2e7127daac7748ce4a90bc32c9277e6a174169
kost <solodkovkm1997@gmail.com>
2023-02-28 21:22:23 +0300

    task42 updated

--- lesson6/task42/Program.cs
+++ lesson6/task42/Program.cs
@@ -1,14 +1,4 @@
-﻿int x = 13;
-
-void Reverse(string[] array)
-{
-    for(int i = 0; i < array.Length/2; i++)
-    {
-        string temp = array[i];
-        array[i] = array[array.Length - i - 1];
-        array[array.Length - i - 1] = temp;
-    }
-}
+﻿int x = 47;
 
 string binary = "";
 
@@ -17,13 +7,9 @@ void ConvertToBin(int x)
     if(x > 0)
     {
         ConvertToBin(x/2);
-        binary += x % 2 + ",";
+        binary = x % 2 + binary;
     }
 }
 
 ConvertToBin(x);
 Console.WriteLine(binary);
-string[] array_void = binary.Split(",");
-Console.WriteLine(string.Join("",array_void));
-Reverse(array_void);
-Console.WriteLine(string.Join("",array_void));
