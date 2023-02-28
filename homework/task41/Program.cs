0845caa9a9399863976a35f9f132c20f446bd29e
kost <solodkovkm1997@gmail.com>
2023-02-28 23:04:06 +0300

    task41

---
+++ homework/task41/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите длинну массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+int count = 0;
+for(int i = 0; i < length; i++)
+{
+    Console.WriteLine("Введите число");
+    if(!int.TryParse(Console.ReadLine(), out int n))
+    {
+        Console.WriteLine("Это не число");
+        i--;
+    }
+    else
+    {
+        array[i] = n;
+        if(array[i] > 0) count++;
+    }
+}
+Console.WriteLine($"[{string.Join(",",array)}] -> {count}");
