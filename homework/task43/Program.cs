1e099fa7fe1f68147bca4b9d122bd2bb13367dc9
kost <solodkovkm1997@gmail.com>
2023-03-01 02:41:27 +0300

    task43 updated

--- homework/task43/Program.cs
+++ homework/task43/Program.cs
@@ -1,19 +1,19 @@
 ﻿using ClassesLibrary;
 
-int b1 = new Print().PrintReadInt("Введите b1");
-int k1 = new Print().PrintReadInt("Введите k1");
-int b2 = new Print().PrintReadInt("Введите b2");
-int k2 = new Print().PrintReadInt("Введите k2");
+int b1 = "Введите b1".PrintGet();
+int k1 = "Введите k1".PrintGet();
+int b2 = "Введите b2".PrintGet();
+int k2 = "Введите k2".PrintGet();
 
 if((b2-b1)%(k1-k2) == 0)
 {
     int x = (b2-b1)/(k1-k2);
     int y = k1 * x + b1;
-    Console.WriteLine($"b1:{b1} k1:{k1} b2:{b2} k2:{k2} -> {x}:{k1 * x + b1}");
+    $"b1:{b1} k1:{k1} b2:{b2} k2:{k2} -> {x}:{k1 * x + b1}".Print();
 } 
 else 
 {
     double x = (double)(b2-b1)/(k1-k2);
     double y = (double)(k1 * x + b1);
-    Console.WriteLine($"b1:{b1} k1:{k1} b2:{b2} k2:{k2} -> {x}:{y}");
+    $"b1:{b1} k1:{k1} b2:{b2} k2:{k2} -> {x}:{y}".Print();
 }
--- test2/Program.cs
+++ test2/Program.cs
@@ -1,10 +1,10 @@
 ﻿using ClassesLibrary;
+const int LENGTH = 10;
+int[] filled_int_array = new ArrayWork().FillArray(LENGTH, -10, 10);
+double[] filled_double_array = new ArrayWork().FillArray(LENGTH, 10.2);
+char[] filled_string_array = new ArrayWork().FillArray(LENGTH);
 
-int[] filled_int_array = new ArrayWork().FillArray(10, -10, 10);
-double[] filled_double_array = new ArrayWork().FillArray(10, 10.2);
-char[] filled_string_array = new ArrayWork().FillArray(10);
-
-Console.WriteLine($"[{string.Join(',', filled_int_array)}]");
-Console.WriteLine($"[{string.Join(',', filled_double_array.Select(x=> Math.Round(x, 2)))}]");
-Console.WriteLine($"[{string.Join(',', filled_string_array)}]");
-
+$"[{string.Join(',', filled_int_array)}]".Print();
+$"[{string.Join(',', filled_double_array.Select(x=> Math.Round(x, 2)))}]".Print();
+$"[{string.Join(',', filled_string_array)}]".Print();
+"Введите число".PrintGet().Print();
