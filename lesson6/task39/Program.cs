f5b58971651d74c89d4a1de348fe996feef5f2b0
kost <solodkovkm1997@gmail.com>
2023-02-28 20:36:11 +0300

    task39

---
+++ lesson6/task39/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassesLibrary;
+
+int[] filled_array = new ArrayWork().FillArray(10, 0, 6);
+
+void Reverse(int[] array)
+{
+    for(int i = 0; i < array.Length/2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+}
+Console.WriteLine($"{string.Join(',',filled_array)}");
+Reverse(filled_array);
+Console.WriteLine($"{string.Join(',',filled_array)}");
