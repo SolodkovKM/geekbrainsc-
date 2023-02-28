ea55ff25411fa2f220e570801ef2c66e271fa0f3
kost <solodkovkm1997@gmail.com>
2023-02-28 22:48:25 +0300

    task45

---
+++ lesson6/task45/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassesLibrary;
+
+int[] array = new ArrayWork().FillArray(10, 0, 10);
+
+int[] rec_array = new int[array.Length];
+void CopyArrayRec(int length)
+{
+    if(length > 0)
+    {
+        rec_array[length-1] = array[length-1];
+        CopyArrayRec(length-1);
+    }
+}
+CopyArrayRec(array.Length);
+array[0] = -10000;
+
+Console.WriteLine(string.Join(",",array));
+Console.WriteLine(string.Join(",", rec_array));
