dc79e55301f164b00a60bf480b35deeda7fcf88a
kost <solodkovkm1997@gmail.com>
2023-02-28 21:55:42 +0300

    task44

---
+++ lesson6/task44/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int f = Convert.ToInt32(Console.ReadLine());
+int[] Fibonacci(int f)
+{
+    if(f <= 2) return new int[] {0, 1};
+    int[] fib = new int[f];
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i = 2; i < f; i++)
+    {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib;
+}
+Console.WriteLine(string.Join(" ", Fibonacci(f)));
