fe3ea1c50b309f19d36a1ec153d4bc6ab03e1124
kost <solodkovkm1997@gmail.com>
2023-02-28 22:48:05 +0300

    task45

---
+++ test2/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassesLibrary;
+
+int[] filled_int_array = new ArrayWork().FillArray(10, -10, 10);
+double[] filled_double_array = new ArrayWork().FillArray(10, 10.2);
+char[] filled_string_array = new ArrayWork().FillArray(10);
+
+Console.WriteLine($"[{string.Join(',', filled_int_array)}]");
+Console.WriteLine($"[{string.Join(',', filled_double_array.Select(x=> Math.Round(x, 2)))}]");
+Console.WriteLine($"[{string.Join(',', filled_string_array)}]");
+
