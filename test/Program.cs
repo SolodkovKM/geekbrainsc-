54300e8ef6a955b97183ecb015c4401ad0962aef
kost <solodkovkm1997@gmail.com>
2023-03-03 00:56:51 +0300

    class updated

---
+++ test/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Person{
+    public class Person
+    {
+        public string name = "Undefined";
+        public int age;
+        public void Print()
+        {
+            Console.WriteLine($"Name: {name} Age: {age}");
+        }
+    }
+}
