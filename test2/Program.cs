ce616d2deb1cf6c674dcc9a03360f721208476dc
kost <solodkovkm1997@gmail.com>
2023-03-03 00:56:42 +0300

    class updated

--- test2/Program.cs
+++ test2/Program.cs
@@ -3,6 +3,7 @@ namespace Test
 {
     class Program
     {
+        const int LENGTH = 10;
         static int Sum(params int[] parameters)
         {
             int result = 0;
@@ -14,11 +15,12 @@ namespace Test
         }
         static void Main(string[] args)
         {
-            const int LENGTH = 10;
             int[] filled_array = new int[LENGTH];
             $"Int Array: [{string.Join(", ", filled_array.FillArray(LENGTH, -10, 10))}]".Print();
             $"Double Array: [{string.Join(", ", filled_array.FillArray(LENGTH, 10).Select(x=> Math.Round(x, 2)))}]".Print();
             $"Char Array: [{string.Join(", ", filled_array.FillArray(LENGTH))}]".Print();
+            5.IsEven().Print();
+            4.IsEven().Print();
         }
     }
 }
