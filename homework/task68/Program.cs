ebb4ed889aeac184d598c912fd2d6c883e27a787
kost <solodkovkm1997@gmail.com>
2023-03-10 22:48:27 +0300

    swminar9HomeTasks

---
+++ homework/task68/Program.cs
@@ -0,0 +1,8 @@
+﻿int Akkerman(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if(n == 0) return Akkerman(m-1, 1);
+    return Akkerman(m-1, Akkerman(m, n-1));
+}
+
+System.Console.WriteLine(Akkerman(3, 5));
