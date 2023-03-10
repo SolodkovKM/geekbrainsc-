d1873bf0e7f13042ba426190f19538d563365058
kost <solodkovkm1997@gmail.com>
2023-03-10 22:51:10 +0300

    task68 updated

--- homework/task68/Program.cs
+++ homework/task68/Program.cs
@@ -1,8 +1,11 @@
 ﻿int Akkerman(int m, int n)
 {
+    if(m < 0 || n < 0) throw new Exception("M or N must be UINT");
     if(m == 0) return n + 1;
     else if(n == 0) return Akkerman(m-1, 1);
     return Akkerman(m-1, Akkerman(m, n-1));
 }
 
-System.Console.WriteLine(Akkerman(3, 5));
+Console.WriteLine(Akkerman(3, 5));
+Console.WriteLine(Akkerman(3, 3));
+Console.WriteLine(Akkerman(3, -1));
